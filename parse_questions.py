14d24ee2462cb3934a3b851fecfffa73987d7b8a
Yulia51188 <sun51188@gmail.com>
2020-10-30 15:45:16 +0300

    Create def parse_questions()

--- parse_questions.py
+++ parse_questions.py
@@ -1,15 +1,29 @@
-questions_file = 'questions/test.txt'
 
-with open(questions_file, 'r', encoding='koi8-r') as file_obj:
-    questions_text = file_obj.read()
+import logging
 
-text_parts = questions_text.split('\n\n')
-questions = [part.split('\n')[1] for part in text_parts 
-    if 'Вопрос' in part.split('\n')[0]]
-answers = [part.split('\n')[1] for part in text_parts 
-    if 'Ответ' in part.split('\n')[0]]   
+logger = logging.getLogger('quiz_bot_logger')
 
-quiz = [{'question': question, 'answer': answer} 
-    for question, answer in zip(questions, answers)]
+def parse_questions(file_path):
+    with open(file_path, 'r', encoding='koi8-r') as file_obj:
+        questions_text = file_obj.read()
 
-print(len(quiz))   
+    text_parts = questions_text.split('\n\n')
+    questions = [part.split('\n')[1] for part in text_parts 
+        if 'Вопрос' in part.split('\n')[0]]
+    answers = [part.split('\n')[1] for part in text_parts 
+        if 'Ответ' in part.split('\n')[0]]   
+
+    quiz = [{'question': question, 'answer': answer} 
+        for question, answer in zip(questions, answers)]
+
+    logger.info(f"Parsed {len(quiz)} questions") 
+    return quiz
+
+
+def main():
+    questions_file = 'questions/test.txt'
+    parse_questions(questions_file)
+
+
+if __name__ == '__main__':
+    main()
