6ab8efefa1c1d59804aab2432d493c79af0f2e15
Yulia51188 <sun51188@gmail.com>
2020-10-30 15:46:01 +0300

    Done step 5: send question on the buttom click

--- tg_bot.py
+++ tg_bot.py
@@ -13,16 +13,26 @@ Press Ctrl-C on the command line or send a signal to the process to stop the
 bot.
 """
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
-from dotenv import load_dotenv
 import os
 
+import random
+
+from dotenv import load_dotenv
+from parse_questions import parse_questions
+from telegram import ReplyKeyboardMarkup
+from telegram import ReplyKeyboardRemove
+from telegram.ext import CommandHandler
+from telegram.ext import Filters
+from telegram.ext import MessageHandler
+from telegram.ext import Updater
+
+import redis
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('quiz_bot_logger')
 
 
 # Define a few command handlers. These usually take the two arguments bot and
@@ -30,6 +40,12 @@ logger = logging.getLogger(__name__)
 def start(bot, update):
     """Send a message when the command /start is issued."""
     update.message.reply_text('Hi!')
+    send_keyboard(bot, update.message.chat_id)
+
+
+def stop(bot, update):
+    """Send a message when the command /start is issued."""
+    remove_keyboard(bot, update.message.chat_id)
 
 
 def help(bot, update):
@@ -47,6 +63,11 @@ def error(bot, update, error):
     logger.warning('Update "%s" caused error "%s"', update, error)
 
 
+def answer(bot, update):
+    if update.message.text == 'Новый вопрос':
+        update.message.reply_text(get_random_question()["question"])
+
+
 def run_echobot(bot_token):
     # Create the EventHandler and pass it your bot's token.
     updater = Updater(bot_token)
@@ -57,9 +78,9 @@ def run_echobot(bot_token):
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
-
+    dp.add_handler(CommandHandler("stop", stop))
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_handler(MessageHandler(Filters.text, answer))
 
     # log all errors
     dp.add_error_handler(error)
@@ -71,6 +92,26 @@ def run_echobot(bot_token):
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
+
+
+def send_keyboard(bot, chat_id):
+    custom_keyboard = [['Новый вопрос', 'Сдаться'], ['Мой счёт']]
+    reply_markup = ReplyKeyboardMarkup(custom_keyboard)
+    bot.send_message(chat_id=chat_id, text="Начинаем викторину!", 
+        reply_markup=reply_markup)
+
+
+def remove_keyboard(bot, chat_id):
+    reply_markup = ReplyKeyboardRemove()
+    bot.send_message(chat_id=chat_id, text='Викторина прервана', 
+        reply_markup=reply_markup)
+
+
+def get_random_question(file_path='Data/test.txt'):
+    quiz = parse_questions(file_path)
+    question = random.choice(quiz)
+    logger.info(question)
+    return(question)
 
 
 def main():
