91b8fc71cf819445086e9ec5c56db54a4fb5f581
Yulia Evseeva <ing1_0481@geo-nv.com>
2020-11-02 10:37:31 +0300

    Repair questions parsing

--- parse_questions.py
+++ parse_questions.py
@@ -2,26 +2,34 @@
 import logging
 
 logger = logging.getLogger('quiz_bot_logger')
+logging.basicConfig(format='%(message)s',
+                    level=logging.DEBUG)
 
 def parse_questions(file_path):
     with open(file_path, 'r', encoding='koi8-r') as file_obj:
         questions_text = file_obj.read()
 
-    text_parts = questions_text.split('\n\n')
-    questions = [part.split('\n')[1] for part in text_parts 
-        if 'Вопрос' in part.split('\n')[0]]
-    answers = [part.split('\n')[1] for part in text_parts 
-        if 'Ответ' in part.split('\n')[0]]   
+    text_parts = questions_text.split('Вопрос')
+    quiz = []
+    for part in text_parts[1:]:
+        subparts = part.split('\n\n')
+        logger.debug(subparts)
+        if len(subparts) > 1:
+            quiz.append({
+                'question': subparts[0].split(':')[1],
+                'answer': subparts[1].split(':')[1],
+            })
 
-    quiz = [{'question': question, 'answer': answer} 
-        for question, answer in zip(questions, answers)]
-
+    for part in quiz:
+        logger.debug(f'Вопрос: {part["question"]}')
+        logger.debug(f'Ответ: {part["answer"]}')        
+        logger.debug('')
     logger.info(f"Parsed {len(quiz)} questions") 
     return quiz
 
 
 def main():
-    questions_file = 'questions/test.txt'
+    questions_file = 'Data/120br.txt'
     parse_questions(questions_file)
 
 
