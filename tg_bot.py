56203e0f5b9863c01b8a4effd6b6a4aac918fb98
Yulia51188 <sun51188@gmail.com>
2020-11-10 16:06:26 +0300

    Done step 9: add ConversationHandler to tg bot

--- tg_bot.py
+++ tg_bot.py
@@ -1,37 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Simple Bot to reply to Telegram messages.
-This program is dedicated to the public domain under the CC0 license.
-This Bot uses the Updater class to handle the bot.
-First, a few handler functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
-
 import logging
 import os
-
 import random
-
 import re
+import redis
 
 from dotenv import load_dotenv
+from enum import Enum
 from functools import partial
 from parse_questions import parse_questions
 from telegram import ReplyKeyboardMarkup
 from telegram import ReplyKeyboardRemove
 from telegram.ext import CommandHandler
+from telegram.ext import ConversationHandler
 from telegram.ext import Filters
 from telegram.ext import MessageHandler
+from telegram.ext import RegexHandler
 from telegram.ext import Updater
 
-import redis
-# Enable logging
+
+class States(Enum):
+    MENU_BUTTON_CLICK = 1
+    ANSWER = 2
+
+
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
@@ -40,30 +34,20 @@ logger = logging.getLogger('quiz_bot_logger')
 TRUE_RESPONSE = "Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»"
 FALSE_RESPONSE = "Неправильно... Попробуешь ещё раз?"
 
-# Define a few command handlers. These usually take the two arguments bot and
-# update. Error handlers also receive the raised TelegramError object in error.
+# MENU_BUTTON_CLICK, ANSWER = range(2)
+
+
 def start(bot, update):
-    """Send a message when the command /start is issued."""
     send_keyboard(bot, update.message.chat_id, 'Начинаем викторину!')
+    return States.MENU_BUTTON_CLICK
 
 
 def stop(bot, update):
-    """Send a message when the command /start is issued."""
     remove_keyboard(bot, update.message.chat_id)
-
-
-def help(bot, update):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
-
-
-def echo(bot, update):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+    return ConversationHandler.END 
 
 
 def error(bot, update, error):
-    """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, error)
 
 
@@ -82,66 +66,32 @@ def validate_answer(full_answer, user_msg):
         return answer == user_answer
 
 
-def answer(bot, update, db):
-    if update.message.text == 'Новый вопрос':
-        new_question = get_random_question()
-        bot_response = new_question["question"]
-        db.set(
-            update.message.chat_id, 
-            new_question["answer"]
-        )
-        logger.info(f"QUIZ ITEM SET:\n{db.get(update.message.chat_id)}")
+def handle_new_question_request(bot, update, db):
+    new_question = get_random_question()
+    bot_response = new_question["question"]
+    send_keyboard(bot, update.message.chat_id, bot_response)    
+    db.set(
+        update.message.chat_id, 
+        new_question["answer"]
+    )
+    logger.info(f"QUIZ ITEM SET:\n{db.get(update.message.chat_id)}")
+    return States.ANSWER
 
-    elif update.message.text == 'Сдаться':
-        bot_response = 'Жаль'
-        pass
-    elif update.message.text == 'Мой счёт':
-        bot_response = 'Твой счёт 0'
-        pass
-    else:
-        quiz_item = db.get(update.message.chat_id)
-        logger.info(f"QUIZ ITEM GET:\n{quiz_item}")
+
+def handle_solution_attempt(bot, update, db):
+    quiz_item = db.get(update.message.chat_id)
+    logger.info(f"QUIZ ITEM GET:\n{quiz_item}")
         
-        is_answer_true = validate_answer(quiz_item,  update.message.text)
-        bot_response = is_answer_true and TRUE_RESPONSE or FALSE_RESPONSE
+    is_answer_true = validate_answer(quiz_item,  update.message.text)
+    bot_response = is_answer_true and TRUE_RESPONSE or FALSE_RESPONSE
         
-        # update.message.reply_text(bot_response)
     send_keyboard(bot, update.message.chat_id, bot_response)
+    return is_answer_true and States.MENU_BUTTON_CLICK or States.ANSWER
 
 
-def run_echobot(bot_token, db_host, db_port, db_password):
-    redis_db = redis.Redis(host=db_host, port=db_port, db=0, 
-        password=db_password, decode_responses=True)
-
-    # Create the EventHandler and pass it your bot's token.
-    updater = Updater(bot_token)
-
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CommandHandler("stop", stop))
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(
-        Filters.text, 
-        partial(
-                answer, 
-                db=redis_db
-            )
-    ))
-
-    # log all errors
-    dp.add_error_handler(error)
-
-    # Start the Bot
-    updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
+def handle_my_points_request(bot, update):
+    send_keyboard(bot, update.message.chat_id, 'Твой счёт 10 баллов')
+    return States.MENU_BUTTON_CLICK
 
 
 def send_keyboard(bot, chat_id, text):
@@ -164,15 +114,44 @@ def get_random_question(file_path='Data/test.txt'):
     return(question)
 
 
+def run_bot(bot_token, db_host, db_port, db_password):
+    redis_db = redis.Redis(host=db_host, port=db_port, db=0, 
+        password=db_password, decode_responses=True)
+
+    updater = Updater(bot_token)
+    dp = updater.dispatcher
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+
+        states={
+            States.MENU_BUTTON_CLICK: [
+                RegexHandler('^Новый вопрос$', 
+                    partial(handle_new_question_request, db=redis_db)),
+                RegexHandler('^Мой счёт$', 
+                    handle_my_points_request),
+                ],
+            States.ANSWER: [MessageHandler(
+                Filters.text, 
+                partial(handle_solution_attempt, db=redis_db)
+            )],
+        },
+        fallbacks=[CommandHandler('stop', stop)]
+    )
+    dp.add_handler(conv_handler)
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+    updater.idle()
+
+
 def main():
     load_dotenv()
     bot_token = os.getenv("TOKEN")
     db_host = os.getenv("DB_HOST")
     db_port = os.getenv("DB_PORT")
     db_password = os.getenv("DB_PASSWORD")
-    run_echobot(bot_token, db_host, db_port, db_password)
-    # print(validate_answer("Голубь и овца (агнец).", "голубь и овца"))
-
+    run_bot(bot_token, db_host, db_port, db_password)
 
 
 if __name__ == '__main__':
